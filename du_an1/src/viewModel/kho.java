4b7c003d8a1b97cc1ddb35a063adebba36a16a0c
PHƯỢNG\Phuong <phuongvtph25658@fpt.edu.vn>
2022-11-20 17:42:55 +0700

    Phượng đại ca

---
+++ du_an1/src/viewModel/kho.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package viewModel;
+
+/**
+ *
+ * @author Phuong
+ */
+public class kho {
+    private String makho;
+    private String manl;
+    private String tennl;
+    private int soluongton;
+
+    public kho() {
+    }
+
+    public kho(String makho, String manl, String tennl, int soluongton) {
+        this.makho = makho;
+        this.manl = manl;
+        this.tennl = tennl;
+        this.soluongton = soluongton;
+    }
+
+    public String getMakho() {
+        return makho;
+    }
+
+    public void setMakho(String makho) {
+        this.makho = makho;
+    }
+
+    public String getManl() {
+        return manl;
+    }
+
+    public void setManl(String manl) {
+        this.manl = manl;
+    }
+
+    public String getTennl() {
+        return tennl;
+    }
+
+    public void setTennl(String tennl) {
+        this.tennl = tennl;
+    }
+
+    public int getSoluongton() {
+        return soluongton;
+    }
+
+    public void setSoluongton(int soluongton) {
+        this.soluongton = soluongton;
+    }
+    
+    
+}
